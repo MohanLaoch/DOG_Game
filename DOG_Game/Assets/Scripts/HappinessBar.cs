a824a783e91bda80b2ab0d6defc1596b11f0c758
James Mohan <c20305011@mytudublin.ie>
2022-03-08 23:37:38 +0000

    Added Happiness UI

---
+++ DOG_Game/Assets/Scripts/HappinessBar.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HappinessBar : MonoBehaviour
+{
+
+    public Slider slider;
+    public Gradient gradient;
+
+    public Image fill;
+
+    // Sets slider to max happiness
+    public void SetMaxHappiness(int happiness)
+    {
+        slider.maxValue = happiness;
+        slider.value = happiness;
+
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    // Sets slider to current happiness
+    public void SetHappiness(int happiness)
+    {
+        slider.value = happiness;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+}
